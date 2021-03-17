22cf532ada0685090353a63a39a2b816ddbcc223
Willian Monteiro <willianmntro@gmail.com>
2021-03-16 22:28:36 -0300

    refactor(index): change default export

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-export { default as date } from './src/date';
-export { default as mask } from './src/mask';
-export { default as number } from './src/number';
-export { default as object } from './src/object';
-export { default as password } from './src/password';
-export { default as string } from './src/string';
-export { default as validator } from './src/validator';
+export { default as dateUtil } from './lib/src/date';
+export { default as maskUtil } from './lib/src/mask';
+export { default as numberUtil } from './lib/src/number';
+export { default as objectUtil } from './lib/src/object';
+export { default as passwordUtil } from './lib/src/password';
+export { default as stringUtil } from './lib/src/string';
+export { default as validatorUtil } from './lib/src/validator';
